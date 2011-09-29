b834f8ec9009de3c5bd4f8f7368061d0c984b859
Thomas Uselman <thomasuselman@gmail.com>
2011-09-28 19:04:08 -0500

    New Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages/contact"
+  root :to => 'pages#home'
+  
+  #get "pages/home"
+
+  #get "pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
