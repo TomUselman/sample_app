3e9d212e70339509301ba39759258dac5ccdaeac
Thomas Uselman <thomasuselman@gmail.com>
2011-09-28 20:32:25 -0500

    Errors

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,23 +2,18 @@ require 'spec_helper'
 
 describe PagesController do
 
-  it "should have a Home page at '/'" do
-    get '/'
-    response.should have_selector('title', :content => "Home")
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
   end
 
-  it "should have a Contact page at '/contact'" do
-    get '/contact'
-    response.should have_selector('title', :content => "Contact")
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
   end
 
-  it "should have an About page at '/about'" do
-    get '/about'
-    response.should have_selector('title', :content => "About")
-  end
-
-  it "should have a Help page at '/help'" do
-    get '/help'
-    response.should have_selector('title', :content => "Help")
-  end
 end
