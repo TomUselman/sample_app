844bf90583c443fd58d83161ccb53d5a236b73ec
Thomas Uselman <thomasuselman@gmail.com>
2011-12-15 16:44:43 -0600

    Working on Tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,7 +13,7 @@ describe "Users" do
           fill_in "Email",        :with => ""
           fill_in "Password",     :with => ""
           fill_in "Confirmation", :with => ""
-          click_button
+          #click_button
           response.should render_template('users/new')
           response.should have_selector("div#error_explanation")
         end.should_not change(User, :count)
@@ -29,7 +29,7 @@ describe "Users" do
           fill_in "Email",        :with => "user@example.com"
           fill_in "Password",     :with => "foobar"
           fill_in "Confirmation", :with => "foobar"
-          click_button
+          #click_button
           response.should have_selector("div.flash.success",
                                         :content => "Welcome")
           response.should render_template('users/show')
